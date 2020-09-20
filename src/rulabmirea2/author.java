d18d5bd0010398d1df3fcb0f6942420f57a38b77
SERGEY <ksergey263@gmail.com>
2020-09-20 17:16:15 +0300

    Initial commit

---
+++ src/rulabmirea2/author.java
@@ -0,0 +1,31 @@
+package rulabmirea2;
+
+import java.lang.*;
+
+public class author
+{
+    private String name;
+    private String email;
+    private char gender;
+
+    public author(String name, String email, char gender){
+        this.name=name;
+        this.email=email;
+        this.gender=gender;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+
+        return email;
+    }
+    public char getGender(){
+
+        return gender;
+    }
+    public String toString(){
+        return ""+this.name+" ("+this.gender+") at "+this.email+"";
+    }
+
+}
---
+++ src/rulabmirea2/testauthor.java
@@ -0,0 +1,8 @@
+package rulabmirea2;
+
+public class testauthor {
+    public static void main(String[] args){
+        author b1 = new author("Puskin","pushkin@yandex.ru",'M');
+        System.out.println(b1);
+    }
+}
